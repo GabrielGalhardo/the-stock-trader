41316fb6ebb8bd65fc8fb6f31befea79c8055146
GabrielGalhardo <galhardogabriel843@gmail.com>
2021-12-21 09:34:00 -0300

    Tela de ações

--- stock-trader/src/components/stocks/Acoes.vue
+++ stock-trader/src/components/stocks/Acoes.vue
@@ -1,12 +1,22 @@
 <template>
-    <h1>Açoes</h1>
+  <v-layout row wrap>
+    <Stock v-for="stock in stocks" :key="stock.id" :stock="stock" />
+  </v-layout>
 </template>
 <script>
-
+import Stock from "./Stock.vue";
 export default {
-//     data() {
-        
-//     },
-}
+  components: { Stock },
+  data() {
+    return {
+      stocks: [
+        { id: 1, name: "BMW", price: 290 },
+        { id: 2, name: "Google", price: 300 },
+        { id: 3, name: "Apple", price: 510 },
+        { id: 4, name: "Twitter", price: 210 },
+      ],
+    };
+  },
+};
 </script>
 
